da5f742bdcfd90b83b2390ea6a43bf4d5a9eeb45
lapek <lapek@yourmum.com>
2023-08-22 23:56:00 +0200

    Add Front and Backend engine

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+from test import get_moti_letter
+
+app = Flask(__name__, template_folder="./templates/")
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/complete/", methods=["POST"])
+def complete_query():
+
+    def find_all_keys_recursive(dictionary):
+        all_keys = set()
+        for key, value in dictionary.items():
+            all_keys.add(key)
+            if isinstance(value, dict):
+                nested_keys = find_all_keys_recursive(value)
+                all_keys.update(nested_keys)
+        return all_keys
+
+    def check_request(request_dict):
+        acceptable_keys = {"language", "job_url", "details", "job_title", "person_name", "person_surname", "person_age", "person_interests", "person_about"}
+
+        all_keys = find_all_keys_recursive(request_dict)
+
+        is_url_correct = request_dict.get("job_url", "").startswith("https://www.pracuj.pl")
+
+        return True if all_keys == acceptable_keys and is_url_correct else False
+
+
+    decoded_details = request.json
+
+    if check_request(decoded_details):
+        url = decoded_details["job_url"]
+        person_details = decoded_details["details"]
+        print(decoded_details, type(decoded_details))
+
+        language = person_details["language"]
+        job_title = person_details["job_title"]
+        person_name = person_details["person_name"]
+        person_surname = person_details["person_surname"]
+        person_age = person_details["person_age"]
+        person_interests = person_details["person_interests"]
+        person_about = person_details["person_about"]
+        
+        letter = get_moti_letter(language, url, job_title, person_name, person_surname, person_age, person_interests, person_about)
+
+        return jsonify({"data": letter})
+    else:
+        return jsonify({"data": "BAD REQUEST"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,78 @@
+import g4f
+import requests
+from bs4 import BeautifulSoup
+import time
+import tiktoken
+
+TOKEN_LIMIT = 4096
+
+PROVIDER = g4f.Provider.DeepAi
+
+
+def count_tokens(text: str) -> int:
+    encoding = tiktoken.get_encoding("cl100k_base")
+
+    return len(encoding.encode(text))
+
+
+def get_company_details(url: str) -> str:
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+        
+    text_elements = soup.find_all(string=True)
+
+    visible_texts = filter(lambda text: not text.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]'], text_elements)
+    extracted_text = ' '.join(visible_texts)
+
+    content = f"Wyekstraktuj dane o firmie z tego tekstu: {extracted_text}"
+
+    response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', provider=PROVIDER, messages=[
+                                        {"role": "user", "content": content}], stream=False)
+
+    return response
+
+
+def get_moti_letter(
+    language: str,
+    job_url: str,
+    job_title: str,
+    person_name: str,
+    person_surname: str,
+    person_age: str,
+    person_interests: str,
+    person_about: str
+) -> str:
+    details = get_company_details(job_url)
+    time.sleep(5)
+
+    about_you = f"You are {person_name} {person_surname}, {person_age} y.o. You are interested in {person_interests}. About you: {person_about}"
+
+    cover_letter_prompt = f"You are applying for a job as a {job_title}, and you want to create a compelling cover letter that showcases your skills, experiences, and enthusiasm for the position. Write a cover letter in {language} that introduces yourself, highlights your relevant achievements and qualifications, and explains why you are the perfect fit for the job. Please make it engaging, persuasive, and tailored to the specific company and job description. Feel free to include any additional information that you think would make you stand out as a top candidate. The company you are applying to is detailed here: {details}."
+
+    complete_prompt = about_you + cover_letter_prompt
+
+    # print("details ", repr(details))
+
+    # moti = f"Napisz list motywacyjny po polsku dla {person_details} ktory stara sie o prace w tej firmie:\n{details}"
+
+    # print(moti)
+    print(f"Tokens in prompt: {count_tokens(complete_prompt)}")
+
+    try:
+        response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', provider=PROVIDER, messages=[
+                                        {"role": "user", "content": complete_prompt}], stream=False)
+    except Exception as e:
+        response = f"{e}"
+
+    return response
+
+
+# pdf_path = "/home/dzony/Downloads/Jakub-Juchnowski.pdf"
+# with open(pdf_path) as f:
+#     pdf = PdfReader(pdf_path)
+
+#     page = pdf.pages[0]
+
+#     print(page.extract_text()) 
+
